0c234f582a1d2a4b6a4af8351553fffbbe7fdee3
murilo henrique <77279094+Murilo4@users.noreply.github.com>
2024-10-23 18:39:22 -0300

    fix firebase

--- user_app/views/login.py
+++ user_app/views/login.py
@@ -13,8 +13,21 @@ from firebase_admin import credentials
 import firebase_admin
 from django.core.cache import cache
 import requests
+import os
 from django.core.exceptions import ValidationError
-cred = credentials.Certificate('user_app/conections/firebase.json')
+cred = credentials.Certificate({
+    "type": "service_account",
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
+})
 firebase_admin.initialize_app(cred)
 
 
