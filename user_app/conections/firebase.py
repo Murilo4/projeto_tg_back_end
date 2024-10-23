6dbb588754c637716f66cdc1b415b3b10c357db8
murilo henrique <77279094+Murilo4@users.noreply.github.com>
2024-10-23 18:08:46 -0300

    fix firebase

--- user_app/conections/firebase.py
+++ user_app/conections/firebase.py
@@ -1,7 +1,18 @@
 import firebase_admin
 from firebase_admin import credentials
 import os
-FIREBASE_KEY = os.getenv('FIREBASE_KEY')
 # Inicialize o Firebase Admin SDK
-cred = credentials.Certificate('FIREBASE_KEY')
+cred = credentials.Certificate({
+    "type": "service_account",
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
+})
 firebase_admin.initialize_app(cred)
