ff417356ca013099efdd35245e6367d7e4828c67
murilo henrique <77279094+Murilo4@users.noreply.github.com>
2024-11-10 13:52:51 -0300

    fix

--- setup/settings.py
+++ setup/settings.py
@@ -63,7 +63,7 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_HEADERS = [
     'jwt_token',  # Permitindo o jwt_token
-   # 'Authorization',  # Se você usa Authorization também
+    'Authorization',  # Se você usa Authorization também
     'Content-Type'  # Outros cabeçalhos comuns
 ]
 
